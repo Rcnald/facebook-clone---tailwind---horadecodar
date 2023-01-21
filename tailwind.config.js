c95591936fe9dec30ebf2603f9ec041465fe826a
Rcnald <ronaldomjunior05@gmail.com>
2023-01-21 13:19:57 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        '30':'7.5rem',
+        '290px':'290px',
+        '396px':'396px',
+        '500px':'500px',
+        '980px':'980px',
+      },
+      screens:{
+        '2md':'990px'
+      },
+
+      fontSize:{
+        'subtitle':'1.7rem',
+      },
+      colors: {
+        'main-gray': '#f0f2f5',
+        'main-blue': '#1877f2',
+        'main-blue-hover': '#166fe5',
+        'main-green': '#42b72a',
+        'main-green-hover': '#36a420'
+      },
+    },
+  },
+  plugins: [],
+}
